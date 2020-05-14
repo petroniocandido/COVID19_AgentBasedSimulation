a1b3051b8f671e8fb9c3454dc6861feeb16cdcd9
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2020-05-14 18:53:30 -0300

    Fixing packaging bugs

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import setuptools
 
 setuptools.setup(
     name='covid_abs',
-    packages=['covid_abs'],
-    version='0.0.1',
+    packages=['covid_abs','covid_abs.network'],
+    version='0.0.2',
     description='COVID-19 Agent Based Simulation',
     long_description='Agent Based Simulation of COVID-19 Health and Economical Effects',
     long_description_content_type="text/markdown",
     author='Petronio Candido L. e Silva',
     author_email='petronio.candido@gmail.com',
     url='https://github.com/petroniocandido/COVID19_AgentBasedSimulation',
-    download_url='https://github.com/PYFTS/pyFTS/archive/pkg1.6.tar.gz',
+    download_url='https://github.com/petroniocandido/COVID19_AgentBasedSimulation',
     keywords=['covid19', 'simulation', 'agent based simulation'],
     classifiers=[
         'Programming Language :: Python :: 3',
